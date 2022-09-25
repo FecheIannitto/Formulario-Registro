064bcd5b540df5a1a4e0e0a73d7050084d730668
FecheIannitto <fmiannitto@gmail.com>
2022-09-25 02:00:30 -0300

    En Proceso

--- reloj.js
+++ reloj.js
@@ -3,30 +3,53 @@ const Stop = document.getElementById("Stop")
 const Reset = document.getElementById("Reset")
 let contadorInterval = null
 let contador = 0
+let inputs, clock, hora, minutos, segundos
 
 
-Start.addEventListener("click", () => {
-    contadorInterval = setInterval(() => {
 
-        console.log(contador)
 
-        contador += 1
+window.addEventListener("load", () => {
 
-    }, 1000)
+    inputs = Array.from(document.getElementsByClassName('number')) //---> Busca los inputs 
+    clock = document.querySelector('.minutero') //---> Busca el reloj
 })
 
 
-Stop.addEventListener("click", () => {
-    clearInterval(contadorInterval)
-    console.log("Stop")
-})
+function arranque() {
+
+    clock.innerHTML = `<p class="number">${hora > 9 ? hora : ('0' + hora)}
+        </p><span>hs.</span><p class="number">${minutos > 9 ? minutos : ('0' + minutos)}
+        </p><span>min.</span><p class="number">${segundos > 9 ? segundos : ('0' + segundos)}
+        </p><span>sec.</span>`
 
 
-Reset.addEventListener("click", () => {
 
-    clearInterval(contadorInterval)
-    contador = 0
+    Start.addEventListener("click", () => {
+        contadorInterval = setInterval(() => {
 
-    console.log("Reset")
-})
 
+            //console.log(contador)
+
+            contador += 1
+
+        }, 1000)
+    })
+
+
+    Stop.addEventListener("click", () => {
+        clearInterval(contadorInterval)
+        //console.log("Stop")
+    })
+
+
+
+
+    Reset.addEventListener("click", () => {
+
+        clearInterval(contadorInterval)
+        contador = 0
+
+        //console.log("Reset")
+    })
+
+}
