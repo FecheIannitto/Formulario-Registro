4b9c17a20151ae3e58f7ee83d1abb5efcbe03d2e
FecheIannitto <fmiannitto@gmail.com>
2022-09-27 01:24:51 -0300

    En Proceso

--- reloj.js
+++ reloj.js
@@ -1,55 +1,79 @@
 const Start = document.getElementById("Start")
 const Stop = document.getElementById("Stop")
 const Reset = document.getElementById("Reset")
+const horas = document.getElementById("horas")
+const minutos = document.getElementById("minutos")
+const segundos = document.getElementById("segundos")
+
 let contadorInterval = null
 let contador = 0
-let inputs, clock, hora, minutos, segundos
+let inputs, clock
 
 
+Start.addEventListener("click", () => {
+    contadorInterval = setInterval(() => {
 
+        setTimer()
+        relojDigital()
 
-window.addEventListener("load", () => {
+        console.log("Start")
 
-    inputs = Array.from(document.getElementsByClassName('number')) //---> Busca los inputs 
-    clock = document.querySelector('.minutero') //---> Busca el reloj
+    }, 1000)
 })
 
 
-function arranque() {
+Stop.addEventListener("click", () => {
+    clearInterval(contadorInterval)
+    console.log("Stop")
+})
 
-    clock.innerHTML = `<p class="number">${hora > 9 ? hora : ('0' + hora)}
-        </p><span>hs.</span><p class="number">${minutos > 9 ? minutos : ('0' + minutos)}
-        </p><span>min.</span><p class="number">${segundos > 9 ? segundos : ('0' + segundos)}
-        </p><span>sec.</span>`
+Reset.addEventListener("click", () => {
 
+    clearInterval(contadorInterval)
 
+    contador = 0
+    segundos.value = "00"
+    minutos.value = "00"
+    horas.value = "00"
 
-    Start.addEventListener("click", () => {
-        contadorInterval = setInterval(() => {
-
-
-            //console.log(contador)
-
-            contador += 1
-
-        }, 1000)
-    })
-
-
-    Stop.addEventListener("click", () => {
-        clearInterval(contadorInterval)
-        //console.log("Stop")
-    })
+    console.log("Reset")
+})
 
 
 
+function setTimer() {
 
-    Reset.addEventListener("click", () => {
-
-        clearInterval(contadorInterval)
-        contador = 0
-
-        //console.log("Reset")
-    })
-
+    segundos.value < 9 ? segundos.value = "0" + (contador += 1) : segundos.value = (contador += 1)
+    //minutos.value < 9 ? minutos.value = "0" + (contador) : minutos.value = (contador)
+    //horas.value < 9 ? horas.value = "0" + (contador += 1) : horas.value = (contador += 1)
+    relojDigital()
 }
+
+
+function relojDigital() {
+
+     /* Si tengo más de 0 segundos, restá segundos */
+    /* Si tengo 0 segundos pero tengo más de 0 minutos, poné segundos en 59 y restale 1 a minutos */
+    /* Si tengo 0 segundos, 0 minutos pero tengo más de 0 horas, poné segundos en 59, minutos en 59 y restale 1 a horas */
+    /* Sino arranca la alarma */
+
+    if (segundos.value > 5) {
+        segundos.value = 0
+    } /*else {
+        if (minutos.value > 0) {
+            segundos.value = 59;
+            minutos.value--;
+        } else {
+            if (horas.value > 0) {
+                segundos.value = 59;
+                minutos.value = 59;
+                horas.value--;
+            } else {
+                //alarm.play();
+            }
+        }
+    }*/
+
+    //setTimer();
+}
+
